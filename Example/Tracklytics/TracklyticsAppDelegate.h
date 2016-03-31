0e29401d33f09ad10b84e959fe8dae406f582f05
Stefvg <stefvangils@MacBook-Pro-van-Stef.local>
2016-03-31 11:47:08 +0200

    Initial commit

---
+++ Example/Tracklytics/TracklyticsAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TracklyticsAppDelegate.h
+//  Tracklytics
+//
+//  Created by Stefvg on 03/31/2016.
+//  Copyright (c) 2016 Stefvg. All rights reserved.
+//
+
+@import UIKit;
+
+@interface TracklyticsAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
