7ac12cdce2633b7b9f02f3a7d19e2ddee393b217
Stefvg <stefvangils@MacBook-Pro-van-Stef.local>
2016-05-24 11:48:57 +0200

    Update documentation

--- Example/Pods/Headers/Private/Tracklytics/GaugeAggregateHelper.h
+++ Example/Pods/Headers/Private/Tracklytics/GaugeAggregateHelper.h
@@ -1,1 +1,24 @@
-../../../../../Pod/Classes/CoreDataObjects/GaugeAggregateHelper.h
+//
+//  Gauge.h
+//  Tracklytics
+//
+//  Created by Stef Van Gils on 14/11/15.
+//  Copyright © 2015 KU Leuven. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "Core.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface GaugeAggregateHelper : Core
+
+// Insert code here to declare functionality of your managed object subclass
+
+-(void) addValue:(NSInteger) value;
+
+@end
+
+NS_ASSUME_NONNULL_END
+
+#import "GaugeAggregateHelper+CoreDataProperties.h"
